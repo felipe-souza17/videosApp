14f818aec84af82b9d4e8dc7cae854759ad7e881
felipe-souza17 <felipesantos.rsouza@gmail.com>
2022-03-09 10:32:23 -0300

    Adicionado Array de filme e interface

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos App';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Liga da Justiça de Zack Snyder (2021)',
+      lancamento: '18/03/2021',
+      duracao: '4h 2m',
+      classificacao: 83,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
+      generos: ['Ação', 'Aventura', 'Fantasia', 'Ficção científica']
+    },
+    {
+      nome: 'Homem-Aranha: Sem Volta Para Casa (2021)',
+      lancamento: '16/12/2021',
+      duracao: '2h 28m',
+      classificacao: 83,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg',
+      generos: ['Ação', 'Aventura', 'Ficção científica']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
